b9813d906724c1b93bf21c51641e9b50225cf4de
melissa-ho <hom39@mcmaster.ca>
2025-01-21 12:53:23 -0500

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,41 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import './globals.css';
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      {/* added background colors and set height to 100vh for now to differentiate the sections */}
+
+
+      {/* Header */}
+      <Header />
+
+      {/* Landing Page */}
+      <section id="home" style={{ height: '100vh', background: '#f0f0f0' }}>
+        <h1>landing page</h1>
+      </section>
+
+      {/* About Section */}
+      <section id="about" style={{ height: '100vh', background: '#dcdcdc' }}>
+        <h1>about</h1>
+      </section>
+
+      {/* Past Events Section */}
+      <section id="services" style={{ height: '100vh', background: '#e0e0e0' }}>
+        <h1>past events</h1>
+      </section>
+
+      {/* Sponsors Section */}
+      <section id="contact" style={{ height: '100vh', background: '#ccc' }}>
+        <h1>sponsors</h1>
+      </section>
+      
+      {/* Footer */}
+      <Footer />
     </div>
+    
   );
 }
