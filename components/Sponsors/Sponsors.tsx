7974f111495c58545c0d8d3487838fb03a28e686
melissa <132623109+melyho@users.noreply.github.com>
2025-02-20 12:14:38 -0500

    Update Sponsors.tsx

--- components/Sponsors/Sponsors.tsx
+++ components/Sponsors/Sponsors.tsx
@@ -13,99 +13,99 @@ export default function Sponsors() {
                 </div>
                 <div className="eplip1"></div>
                 <div className="eplip2"></div>
-                <div className="sponsors-l">
-                    <div className="sponsor-box">
+                <div className="spons-l">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 34.png" width="90%"/>
                     </div>
                         
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/Group 272.png" width="90%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/Mask group.png" width="85%"/>  
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/theKapCo 1.png" width="100%"/>
                     </div>
                 </div>
                 <div className="sponsors-m">
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 22.png" width="90%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 66.png" width="70%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 33.png" width="85%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 67.png" width="80%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 46.png" width="85%"/>
                     </div>
                 </div>
                 <div className='sponsors-sm'>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/Group 153.png" width="95%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 47.png" width="80%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 26.png" width="80%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/Group 154.png" width="90%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 68.png" width="90%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/Group 150.png" width="90%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 56.png" width="90%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 30.png" width="85%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 25.png" width="90%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 50.png" width="95%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 31.png" width="70%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 28.png" width="85%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 32.png" width="85%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 52.png" width="85%"/>
                     </div>
                 </div>
                 <div className="sponsors-sm-last">
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 53.png" width="80%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 55.png" width="70%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 54.png" width="95%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/Vue School logo 2.png" width="90%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 23.png" width="85%"/>
                     </div>
-                    <div className="sponsor-box">
+                    <div className="spons-box">
                         <img src="/sponsors/sponsor-logos/image 27.png" width="90%"/>
                     </div>    
                 </div>
