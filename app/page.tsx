5ffd4987aa261f5bdccedfe4ba231d84520a54cd
melyho <homelissa1@gmail.com>
2025-05-19 16:50:12 -0400

    new website - blank

--- app/page.tsx
+++ app/page.tsx
@@ -13,23 +13,28 @@ export default function Home() {
       {/* Header */}
       <Header />
 
-      {/* Landing Page */}
-      <section id="home" style={{ height: '100vh', background: '#f0f0f0' }}>
+      {/* Hero Section */}
+      <section id="hero" style={{ height: '100vh' }}>
         <h1>landing page</h1>
       </section>
 
       {/* About Section */}
-      <section id="about" style={{ height: '100vh', background: '#dcdcdc' }}>
+      <section id="about" style={{ }}>
         <h1>about</h1>
       </section>
 
-      {/* Past Events Section */}
-      <section id="services" style={{ height: '100vh', background: '#e0e0e0' }}>
+      {/* Statistics Section */}
+      <section id="stats" style={{}}>
         <h1>past events</h1>
       </section>
 
-      {/* Sponsors Section */}
-      <section id="contact" style={{ height: '100vh', background: '#ccc' }}>
+      {/* Gallery Section */}
+      <section id="gallery" style={{}}>
+        <h1>sponsors</h1>
+      </section>
+
+      {/* FAQ Section */}
+      <section id="faq" style={{}}>
         <h1>sponsors</h1>
       </section>
       
